66f21f2981d9c0d48d6f2bac83379c4993227863
Mubeen142 <58806240+Mubeen142@users.noreply.github.com>
2024-10-16 16:33:41 +0200

    Update

--- Service.php
+++ Service.php
@@ -2,6 +2,8 @@
 
 namespace App\Services\Cpanel;
 
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 use App\Services\ServiceInterface;
 use App\Models\Package;
 use App\Models\Order;
@@ -47,7 +49,30 @@ class Service implements ServiceInterface
      */
     public static function setConfig(): array
     {
-        return [];
+        return [
+            [
+                "col" => "col-12",
+                "key" => "cpanel::hostname",
+                "name" => "CPanel Hostname",
+                "description" => "Enter your CPanel hostname",
+                "type" => "url",
+                "rules" => ['required', 'active_url'], // laravel validation rules
+            ],
+            [
+                "key" => "cpanel::api_user",
+                "name" => "CPanel API User",
+                "description" => "Enter your CPanel API User",
+                "type" => "text",
+                "rules" => ['required'], // laravel validation rules
+            ],
+            [
+                "key" => "encrypted::cpanel::api_token",
+                "name" => "CPanel Api Token",
+                "description" => "Enter your CPanel API Token",
+                "type" => "password",
+                "rules" => ['required'], // laravel validation rules
+            ],
+        ];
     }
 
     /**
@@ -60,7 +85,23 @@ class Service implements ServiceInterface
      */
     public static function setPackageConfig(Package $package): array
     {
-        return [];
+        $packageList = Service::api('/listpkgs')['package'] ?? [];
+
+        $packages = collect($packageList)->mapWithKeys(function($package) {
+            return [$package['name'] => $package['name']];
+        });
+
+        return [
+            [
+                "col" => "col-12",
+                "key" => "package",
+                "name" => "CPanel Package",
+                "description" => "Select the CPanel Package that belongs to this Package",
+                "type" => "select",
+                "options" => $packages,
+                "rules" => ['required'], // laravel validation rules
+            ],
+        ];
     }
 
     /**
@@ -86,6 +127,18 @@ class Service implements ServiceInterface
         return [];    
     }
 
+    public function testConnection()
+    {
+        // using api list all packages
+        try {
+            $response = Service::api('/listpkgs');
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', $e->getMessage());
+        }
+
+        return redirect()->back()->with('success', 'Successfully connected to CPanel');
+    }
+
     /**
      * This function is responsible for creating an instance of the
      * service. This can be anything such as a server, vps or any other instance.
@@ -94,7 +147,52 @@ class Service implements ServiceInterface
      */
     public function create(array $data = [])
     {
-        return [];
+        $order = $this->order;
+        $user = $order->user;
+        $package = $order->package;
+
+        $data = [
+            'domain' => $order->domain ?? $user->username . '.com',
+            'username' => substr($user->username, 0, 6) . rand(10, 999), // get first 6 chars and add random number
+            'contactemail' => $user->email,
+            'password' => Str::random(12),
+            'plan' => $package->data('package', 'default'),
+        ];
+
+        // make api call to create cpanel account
+        $response = Service::api('/createacct', $data);
+
+        $order->createExternalUser([
+            'external_id' => 1, // optional
+            'username' => $data['username'],
+            'password' => $data['password'],
+            'data' => [], // Additional data about the user as an array (optional)
+         ]);
+    }
+
+    public static function api($endpoint, $data = [], $method = 'GET')
+    {
+        $hostname = settings('cpanel::hostname') . '/json-api/';
+        $apiUsername = settings('cpanel::api_user');
+        $apiToken = settings('encrypted::cpanel::api_token');
+
+        $response = Http::withHeaders([
+            'Authorization' => "whm {$apiUsername}:{$apiToken}",
+        ])->$method($hostname . $endpoint, $data);
+
+        if($response->failed()) {
+            if($response->status() == 404) {
+                throw new \Exception("404 Not Found | Invalid API endpoint {$endpoint} on host {$hostname}");
+            }
+
+            if($response->status() == 403) {
+                throw new \Exception("403 Forbidden | Invalid API Token or User. <br> <br> Ensure the API token is correct and not expired and the user has the required permissions");
+            }
+
+            dd($response, $response->json(), $response->status());
+        }
+
+        return $response;
     }
 
     /**
